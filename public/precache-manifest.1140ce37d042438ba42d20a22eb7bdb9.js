7dd295073e389ac4dc8d5fcc812b32a3493c14d6
ctbarrel <ctbarrel@gmail.com>
2020-10-09 09:38:04 -0600

    heroku hosting

---
+++ public/precache-manifest.1140ce37d042438ba42d20a22eb7bdb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "803ae7d12dbdbbdccd78a5330c4997e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "404af6bb4d055782dd18",
+    "url": "/static/css/main.2644602b.chunk.css"
+  },
+  {
+    "revision": "40376733618d5c4e32cf",
+    "url": "/static/js/2.1dfb5609.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.1dfb5609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "404af6bb4d055782dd18",
+    "url": "/static/js/main.e911d71b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
